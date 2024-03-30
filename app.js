056d9067e4fc7a89bd64fc131bf86cf0e16cf06a
DominicBarnett <dominic.barnett@icloud.com>
2024-03-30 14:05:40 -0700

    User can see single events

--- app.js
+++ app.js
@@ -36,13 +36,24 @@ app.get('/', (req, res) => {
 app.get('/events/new', (req, res) => {
     res.render('events-new', {});
   })
-  // CREATE
+// CREATE
 app.post('/events', (req, res) => {
-    models.Event.create(req.body).then(event => {
-      res.redirect(`/`);
-    }).catch((err) => {
-      console.log(err)
-    });
+  models.Event.create(req.body).then(event => {
+    // Redirect to events/:id
+    res.redirect(`/events/${event.id}`)
+  }).catch((err) => {
+    console.log(err)
+  });
+})
+// SHOW
+app.get('/events/:id', (req, res) => {
+  // Search for the event by its id that was passed in via req.params
+  models.Event.findByPk(req.params.id).then((event) => {
+    // If the id is for a valid event, show it
+    res.render('events-show', { event: event })
+  }).catch((err) => {
+    // if they id was for an event not in our db, log an error
+    console.log(err.message);
   })
-
+})
 app.listen(3000);
