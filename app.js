dd878af11366faf9475866d4b098c8c5440fde89
DominicBarnett <dominic.barnett@icloud.com>
2024-03-30 09:04:46 -0700

    Users can create events

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // Initialize express
 const express = require('express')
 const app = express()
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+const models = require('./db/models');
 // require handlebars
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars')
@@ -24,9 +26,23 @@ var events = [
     { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
   ]
   
-  // INDEX
-  app.get('/', (req, res) => {
-    res.render('events-index', { events: events });
+// Index
+app.get('/', (req, res) => {
+    models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+      res.render('events-index', { events: events });
+    })
+  })
+  // NEW
+app.get('/events/new', (req, res) => {
+    res.render('events-new', {});
+  })
+  // CREATE
+app.post('/events', (req, res) => {
+    models.Event.create(req.body).then(event => {
+      res.redirect(`/`);
+    }).catch((err) => {
+      console.log(err)
+    });
   })
 
 app.listen(3000);
